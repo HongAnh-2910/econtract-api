37c0d7ada2eeef6d2735a60cd72e78cce19c7cf8
trung <trungnc.hn@havaz.vn>
2023-10-16 11:20:04 +0700

    update

--- app/Http/Controllers/Api/V1/Contract/ContractController.php
+++ app/Http/Controllers/Api/V1/Contract/ContractController.php
@@ -50,40 +50,41 @@ class ContractController extends Controller
 
     public function store(StoreContractRequest $request)
     {
-        $randomNumber = rand(0, 999);
-        $styleNumber = str_pad($randomNumber, 3, '0', STR_PAD_LEFT);
-        $customerCodeRandom = '01GTKT0-' . $styleNumber;
-        $requestContract = $request->except(['files', 'signatures', 'signatures_follow']);
-        $signatures = $request->input('signatures', []);
-        $signaturesFollow = $request->input('signatures_follow', []);
-        $signaturesFollow = collect($signaturesFollow)->map(function ($item) {
+        $randomNumber       = rand(0, 999);
+        $styleNumber        = str_pad($randomNumber, 3, '0', STR_PAD_LEFT);
+        $customerCodeRandom = '01GTKT0-'.$styleNumber;
+        $requestContract    = $request->except(['files', 'signatures', 'signatures_follow']);
+        $signatures         = $request->input('signatures', []);
+        $signaturesFollow   = $request->input('signatures_follow', []);
+        $signaturesFollow   = collect($signaturesFollow)->map(function ($item) {
             $item['token'] = Str::random(40);
             return $item;
         });
-        $nameFiles = [];
+        $nameFiles          = [];
         DB::beginTransaction();
         try {
-            $requestContract['code'] = $customerCodeRandom;
-            $requestContract['status'] = ContractStatus::CANCELED;
+            $requestContract['code']    = $customerCodeRandom;
+            $requestContract['status']  = ContractStatus::CANCELED;
             $requestContract['user_id'] = Auth::id();
-            $requestContract['slug'] = Str::slug($customerCodeRandom . '-' . $requestContract['name_customer'], '-');
-            $contract = Contract::create($requestContract);
-            $uploadFileCreateContract = app()->make(FileServiceInterface::class);
+            $requestContract['slug']    = Str::slug($customerCodeRandom.'-'.$requestContract['name_customer'], '-');
+            $contract                   = Contract::create($requestContract);
+            $uploadFileCreateContract   = app()->make(FileServiceInterface::class);
             if ($request->hasFile('files')) {
-                $dataFile = $uploadFileCreateContract->uploadMultipleFileAndCreateDatabase($request,
+                $dataFile   = $uploadFileCreateContract->uploadMultipleFileAndCreateDatabase($request,
                     'upload_contract', $contract->id);
-                $nameFiles = $dataFile['nameFiles'];
-                $fileIds = $dataFile['fileIds'];
+                $nameFiles  = $dataFile['nameFiles'];
+                $fileIds    = $dataFile['fileIds'];
                 $dataAttach = [];
                 foreach ($nameFiles as $key => $name) {
-                    $dataAttach[$fileIds[$key]] = ['base64' => "data:@file/pdf;base64," . base64_encode(file_get_contents(storage_path('app/public/files/' . $name)))];
+                    $dataAttach[$fileIds[$key]] = ['base64' => "data:@file/pdf;base64,".base64_encode(file_get_contents(storage_path('app/public/files/'.$name)))];
                 }
                 $contract->files()->attach($dataAttach);
                 $contract->signatures()->createMany($signatures);
-                $contract->follow()->createMany($signaturesFollow);
+                $contract->follows()->createMany($signaturesFollow);
             }
             DB::commit();
-            return $contract;
+            $contract = $contract->load('files' ,'signatures' ,'follows' ,'banking' ,'user');
+            return new ContractResource($contract);
         } catch (Exception $exception) {
             DB::rollBack();
             if (count($nameFiles) > 0) {
